8f0f3e9d8cd9db8f701885efc68d451695961f2b
Maksimov86 <tylerbk666@gmail.com>
2024-10-02 09:50:55 -0400

    added code to Register.jsx

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Register.css";
+import user_icon from "../assets/person.png"
+import email_icon from "../assets/email.png"
+import password_icon from "../assets/password.png"
+import close_icon from "../assets/close.png"
+
+const Register = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const gohome = () => {
+        window.location.href = window.location.origin;
+    }
+
+    const register = async (e) => {
+        
+    }
+}
