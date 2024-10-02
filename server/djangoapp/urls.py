de2be55e6d1e0aeba402ea729cd07c32d371659c
Maksimov86 <tylerbk666@gmail.com>
2024-10-02 09:27:21 -0400

    added code to views.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path('registration', views.registration_request, name='registration'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout')
