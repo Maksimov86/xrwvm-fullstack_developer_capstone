d2aa9112d73c89a9c3f96e66c21bf310b8afe06a
Maksimov86 <tylerbk666@gmail.com>
2024-10-08 16:11:23 -0400

    Various fixes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,14 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://tylerbk666-8000.theiadockernext-1-labs-\
-                 prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-                 'https://tylerbk666-8000.theiadockernext-1-labs-\
-                 prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://tylerbk666-8000.theiadockernext-\
-                        1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-                        'https://tylerbk666-8000.theiadockernext-\
-                        1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://tylerbk666-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://tylerbk666-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -101,8 +95,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
